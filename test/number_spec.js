327d9605807505c443f5d34188bb34a7af47935d
Calin <calinilielaur@gmail.com>
2016-04-02 13:55:44 +0200

    removed grunt and meteor related resources; using mocha for testing

--- test/number_spec.js
+++ test/number_spec.js
@@ -1,27 +1,30 @@
+const VMasker = require('../lib/vanilla-masker');
+const expect = require('chai').expect;
+
 describe("VanillaMasker.maskNumber", function() {
 
   it('throws error: "VanillaMasker: There is no element to bind." when element is undefined', function() {
     expect(function() {
       VMasker(undefined).maskNumber();
-    }).toThrow(new Error('VanillaMasker: There is no element to bind.'));
+    }).to.throw(/VanillaMasker: There is no element to bind./);
   });
 
   it('throws error: "VanillaMasker: There is no element to bind." when element is null', function() {
     expect(function() {
       VMasker(null).maskNumber();
-    }).toThrow(new Error('VanillaMasker: There is no element to bind.'));
+    }).to.throw(/VanillaMasker: There is no element to bind./);
   });
 
   it('throws error: "VanillaMasker: There is no element to bind." when element is blank', function() {
     expect(function() {
       VMasker("").maskNumber();
-    }).toThrow(new Error('VanillaMasker: There is no element to bind.'));
+    }).to.throw(/VanillaMasker: There is no element to bind./);
   });
 
   it('does not throw error when element is empty array', function() {
     expect(function() {
       VMasker([]).maskNumber();
-    }).not.toThrow();
+    }).not.to.throw();
   });
 
 });
@@ -29,19 +32,19 @@ describe("VanillaMasker.maskNumber", function() {
 describe("VanillaMasker.toNumber", function() {
 
   it('returns 1000 number when input is 1000', function() {
-    expect(VMasker.toNumber(1000)).toEqual('1000');
+    expect(VMasker.toNumber(1000)).to.equal('1000');
   });
 
   it('returns 100000 number when input is 1a0b0c000', function() {
-    expect(VMasker.toNumber('1a0b0c000')).toEqual('100000');
+    expect(VMasker.toNumber('1a0b0c000')).to.equal('100000');
   });
 
   it('returns 10 number when input is 1-0', function() {
-    expect(VMasker.toNumber('1-0')).toEqual('10');
+    expect(VMasker.toNumber('1-0')).to.equal('10');
   });
 
   it('returns -10 number when input is -10', function() {
-    expect(VMasker.toNumber('-10')).toEqual('-10');
+    expect(VMasker.toNumber('-10')).to.equal('-10');
   });
 
 });
